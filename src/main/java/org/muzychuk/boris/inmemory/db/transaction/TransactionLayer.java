63b0a1836b07e5b0ac75b0cb5049e69597be779e
Boris <bmuzychuk@textback.io>
2026-03-02 16:47:04 +0300

    inmemorydbtransaction: fix final field

--- src/main/java/org/muzychuk/boris/inmemory/db/transaction/TransactionLayer.java
+++ src/main/java/org/muzychuk/boris/inmemory/db/transaction/TransactionLayer.java
@@ -7,7 +7,7 @@ public class TransactionLayer {
     // Хранит изменения текущей транзакции
     // key → Optional<String> (Optional.empty() = DELETE)
     private final Map<String, Optional<String>> changes;
-    private Map<String, Integer> valueStatistics;
+    private final Map<String, Integer> valueStatistics;
 
     TransactionLayer() {
         this.changes = new HashMap<>();
