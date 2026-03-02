0944b4180b7d33c366cc6235b9a1ed538fc39ea9
Boris <bmuzychuk@textback.io>
2026-03-02 10:56:49 +0300

    inmemorydbtransaction: add test

--- src/test/java/org/muzychuk/boris/inmemory/db/transaction/InMemoryKeyValueStoreTest.java
+++ src/test/java/org/muzychuk/boris/inmemory/db/transaction/InMemoryKeyValueStoreTest.java
@@ -6,10 +6,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class InMemoryKeyValueStoreTest {
 
+    private final InMemoryKeyValueStore keyValueStore = new InMemoryKeyValueStore();
+
     @Test
-    void set() {
-        InMemoryKeyValueStore keyValueStore = new InMemoryKeyValueStore();
+    void create() {
         keyValueStore.set("test key", "test value");
         assertEquals("test value", keyValueStore.get("test key"));
     }
+
+    @Test
+    void replace() {
+        keyValueStore.set("key", "value 1");
+        keyValueStore.set("key", "value 2");
+        assertEquals("value 2", keyValueStore.get("key"));
+    }
+
 }
