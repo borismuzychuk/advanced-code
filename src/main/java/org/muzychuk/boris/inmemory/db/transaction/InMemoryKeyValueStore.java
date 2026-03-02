6da53b09e39e34e66b892c4ba5561a25e2997cad
Boris <bmuzychuk@textback.io>
2026-03-02 14:43:15 +0300

    inmemorydbtransaction: add code

--- src/main/java/org/muzychuk/boris/inmemory/db/transaction/InMemoryKeyValueStore.java
+++ src/main/java/org/muzychuk/boris/inmemory/db/transaction/InMemoryKeyValueStore.java
@@ -9,16 +9,19 @@ public class InMemoryKeyValueStore implements KeyValueStore {
 
     private final Map<String, String> keyValueStorage;
     private final Deque<TransactionLayer> transactionLayers;
+    private final Map<String, Integer> valueStatistics;
 
     public InMemoryKeyValueStore() {
         this.keyValueStorage = new HashMap<>();
         this.transactionLayers = new LinkedList<>();
+        this.valueStatistics = new HashMap<>();
     }
 
     @Override
     public void set(String key, String value) {
         if (transactionLayers.isEmpty()) {
             keyValueStorage.put(key, value);
+            valueStatistics.merge(value, 1, Integer::sum);
         } else {
             TransactionLayer transaction = transactionLayers.peek();
             transaction.put(key, value);
@@ -37,11 +40,24 @@ public class InMemoryKeyValueStore implements KeyValueStore {
     // TODO сделать транзакционным
     @Override
     public boolean delete(String key) {
-        return keyValueStorage.remove(key) != null;
+        if (transactionLayers.isEmpty()) {
+            String removed = keyValueStorage.remove(key);
+            boolean removingResult = removed != null;
+            if (removingResult) {
+                valueStatistics.put(removed, valueStatistics.get(removed) - 1);
+            }
+            return removingResult;
+        } else {
+            return transactionLayers.peek().delete(key);
+        }
     }
 
     @Override
     public int count(String value) {
+        if (transactionLayers.isEmpty()) {
+            Integer count = valueStatistics.get(value);
+            return count == null ? 0 : count;
+        }
         return 0;
     }
 
