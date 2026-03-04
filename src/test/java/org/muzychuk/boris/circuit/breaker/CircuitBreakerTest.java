17f33556aa43c40c59b58cc122d30862fc950fd9
Boris <bmuzychuk@textback.io>
2026-03-04 13:22:36 +0300

    add test

--- src/test/java/org/muzychuk/boris/circuit/breaker/CircuitBreakerTest.java
+++ src/test/java/org/muzychuk/boris/circuit/breaker/CircuitBreakerTest.java
@@ -13,6 +13,7 @@ import java.time.Instant;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.muzychuk.boris.circuit.breaker.domain.CircuitBreakerState.CLOSED;
+import static org.muzychuk.boris.circuit.breaker.domain.CircuitBreakerState.OPEN;
 
 class CircuitBreakerTest {
 
@@ -83,5 +84,53 @@ class CircuitBreakerTest {
         assertEquals(3, circuitBreaker.getMetrics().successCount());
         assertEquals(2, circuitBreaker.getMetrics().failureCount());
         assertEquals(40, circuitBreaker.getMetrics().failurePercentage());
+    }
+
+    @Test
+    void whenPercentErrorsMoreThenFailureThreshold_ThenCircuitBreakerSwitchStateToOpen() {
+        CircuitBreakerMetricsHolder metricsHolder = new CircuitBreakerMetricsHolderImpl(new CircuitBreakerMetrics(
+                CLOSED,
+                0,
+                0,
+                0,
+                0,
+                0
+        ));
+        CircuitBrakerStateContext context = new CircuitBrakerStateContext(metricsHolder,
+                new CircuitBreakerConfig(5, 60, Duration.ofSeconds(30), 2, 2));
+        CircuitBreaker circuitBreaker = new CircuitBreaker(context);
+        RequestSender requestSender = new RequestSender();
+
+        CircuitBreakerResult<Response> resultS1 = circuitBreaker.execute(
+                requestSender::successRequest,
+                () -> new Response("FAILED"),
+                Instant.now());
+
+        CircuitBreakerResult<Response> resultS2 = circuitBreaker.execute(
+                requestSender::successRequest,
+                () -> new Response("FAILED"),
+                Instant.now());
+
+        CircuitBreakerResult<Response> resultF1 = circuitBreaker.execute(
+                requestSender::failRequest,
+                () -> new Response("FAILED"),
+                Instant.now());
+
+        CircuitBreakerResult<Response> resultF4 = circuitBreaker.execute(
+                requestSender::failRequest,
+                () -> new Response("FAILED"),
+                Instant.now());
+
+        CircuitBreakerResult<Response> resultF5 = circuitBreaker.execute(
+                requestSender::failRequest,
+                () -> new Response("FAILED"),
+                Instant.now());
+        assertEquals("FAILED", resultF5.value().status());
+        assertEquals(OPEN, circuitBreaker.getState());
+        assertEquals(5, circuitBreaker.getMetrics().totalCalls());
+        assertEquals(2, circuitBreaker.getMetrics().successCount());
+        assertEquals(3, circuitBreaker.getMetrics().failureCount());
+        assertEquals(60, circuitBreaker.getMetrics().failurePercentage());
+
     }
 }
